9253f0e3d0b6a90c8801f8715f4ee3b1569a8d55
Ruslan Tkachenko <Ruslan_Tkachenko@epam.com>
2023-10-10 14:56:20 +0300

    Add .gitignore

---
+++ Context/GeneralContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Modals;
+
+namespace Context
+{
+    public class GeneralContext
+    {
+        public static string ResponseStatus { get; set; }
+
+        public static Pet Pet {get; set;}
+
+        public static List<Pet> Pets {get; set;}
+    }
+}
---
+++ TestData/Pet.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace Modals
+{
+    public class Pet
+    {
+        public long Id { get; set; }
+        public PetCategory Category { get; set; }
+        public string Name { get; set; }
+        public List<string> PhotoUrls { get; set; }
+        public List<Tag> Tags { get; set; }
+        public string Status { get; set; }
+
+        public string GetPropertyValue(string propName) => this.GetType().GetProperty(propName).GetValue(this, null).ToString();
+
+        public void SetPropertyValue(string propName, string propValue) => this.GetType().GetProperty(propName).SetValue(this, propValue);
+    }
+}
